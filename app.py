e83dbf33fa57e3eaabab452e718d2a42a1e6de85
Ompitroda55 <ompitroda55@gmail.com>
2024-02-05 17:28:38 +0530

    good

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('home_alt.html')
+def flashCardApp():
+    categories = ["Literature", "Mathematics", "Science", "History", "Geography", "Computer Science", "Art", "Music", "Health", "Business", "Test Preparation", "Miscellaneous"]
+    return render_template('flash_cards.html', categories=categories)
 
 if __name__ == '__main__':
     app.run()

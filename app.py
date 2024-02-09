83f1a4adef01cfe2c259e04adfac4185395199e8
Ompitroda55 <ompitroda55@gmail.com>
2024-02-09 22:10:32 +0530

    nothing update

--- app.py
+++ app.py
@@ -3,145 +3,19 @@ from bson.objectid import ObjectId
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from flask import Flask, jsonify, render_template, request, redirect, url_for
-# from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
-# run_with_ngrok(app)
 
-# MongoDB Stuff
+# MongoDB Stuff goes here
 uri = "mongodb+srv://admin:admin@learnsynccluster.el71bgq.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client["LearnSyncDatabase"]
 
+# 
+# Supporting Functions for Front-end & other Main Func's()
+# 
 
-from flask import Flask, request, render_template
-
-app = Flask(__name__)
-
-#
-# Code for Login and Homepage
-#
-@app.route('/check-credentials', methods=['POST'])
-def checkCredentials():
-    collection = db["users"]
-    username = request.form.get('username')
-    password = request.form.get('password')
-    print("User password : " + password, username)
-    user = collection.find_one({'username': username})
-    # print(user['password'])
-    if user:
-        if password == user['password']:
-            return jsonify({'message': 'Valid Creds'}), 200
-        else:
-            return jsonify({'message': 'Incorrect Password'}), 400
-  
-    else:
-        return jsonify({'message': 'Username not available'}), 400
-
-# Code to handle login request
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    collection = db["users"]
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        print(username, password)
-        user = collection.find_one({'username': username})
-        requests = get_requests_for_receiver(str(user['_id']))
-        print(requests)
-        return render_template('dashboard.html', user=user, requests=requests)
-    else:
-        return render_template('home_alt.html')
-
-# Code for inserting flashcard created by user
-
-@app.route('/insert-flash-card', methods=['POST'])
-def createFlashCard():
-    data = []
-    name = request.form['name']
-    subject = request.form['subject']
-    questions = request.form.getlist('question[]')
-    answers = request.form.getlist('answer[]')
-    for i in range(len(min(questions,answers))):
-        data.append((questions[i], answers[i]))
-    print(data)
-    createFlashCard(name, subject, data)
-    return redirect(url_for('flashCardApp'))
-
-# Helper function to create flashcard in DB
-
-def createFlashCard(name, category, flashcard_data):
-    collection = db["flashcards"]
-    new_flashcard_pack = {
-        "name" : name,
-        "category" : category,
-        # "created_by": ObjectId("your_user_object_id"),  # Replace with actual ObjectId of the user
-        "created_at": datetime.utcnow().strftime('%Y-%m-%d'),
-        "times_opened": 0,
-        "flashcard_data": flashcard_data
-    }
-    insert_result = collection.insert_one(new_flashcard_pack)
-
-    # Retrieve ObjectId of inserted document
-    id = insert_result.inserted_id
-
-    return id
-
-
-# Main Entry Point
-
-@app.route('/')
-def index():
-    return render_template('home_alt.html')
-
-#
-# User Signup Stuff
-#
-
-@app.route('/check-username-availability', methods=['POST'])
-def check_username_available():
-    collection = db["users"]
-    username = request.form.get('username')
-    user = collection.find_one({'username': username})
-    if user:
-        return jsonify({'message': 'Username already exists'}), 409
-    else:
-        return jsonify({'message': 'Username available'}), 200
-
-@app.route('/check-username', methods=['POST'])
-def check_username():
-    collection = db["users"]
-    username = request.form.get('username')
-    user = collection.find_one({'username': username})
-    if user:
-        return jsonify({'message': 'Username available'}), 200
-    else:
-        return jsonify({'message': 'Username do not exist'}), 409
-
-# Social Stuff
-    
-# Fuction to Create Varous types of request
-
-def createRequest(from_user, to_user, type):
-    request_types = {
-        0:'Friend Request',
-        1:'Streak Request',
-        2:'Invite To Group'
-        }
-    collection = db['requests']
-    new_request = {
-        "sender": from_user,
-        "receiver": to_user,
-        "type" : request_types[type],
-        "status": "pending"
-    }
-    new_request = collection.insert_one(new_request)
-    print(new_request)
-    return new_request
-
-# Function to Create user in DB
-
+# Function for creating user
 def createUser(username, password):
     collection = db["users"]
     
@@ -163,45 +37,80 @@ def createUser(username, password):
     new_user = collection.insert_one(new_user)
     return new_user
 
+# Fuction to Create Varous types of request
+def createRequest(from_user, to_user, type):
+    request_types = {
+        0:'Friend Request',
+        1:'Streak Request',
+        2:'Invite To Group'
+        }
+    collection = db['requests']
+    new_request = {
+        "sender": from_user,
+        "receiver": to_user,
+        "type" : request_types[type],
+        "status": "pending"
+    }
+    new_request = collection.insert_one(new_request)
+    print(new_request)
+    return new_request
 
-# Function to Handle Signup
-
-@app.route('/sign-up', methods=['POST','GET'])
-def signup():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = createUser(username, password)
-        user_id = str(user.inserted_id)
-        user = fetch_user_by_id(user_id)
-        print(user)
-        return render_template('home_alt.html')
+@app.route('/check-username-availability', methods=['POST'])
+def check_username_available():
+    collection = db["users"]
+    username = request.form.get('username')
+    user = collection.find_one({'username': username})
+    if user:
+        return jsonify({'message': 'Username already exists'}), 409
     else:
-        return render_template('signup.html')
+        return jsonify({'message': 'Username available'}), 200
 
-#
-# All code stuff for Flashcards goes here
-#
+@app.route('/check-username', methods=['POST'])
+def check_username():
+    collection = db["users"]
+    username = request.form.get('username')
+    user = collection.find_one({'username': username})
+    if user:
+        return jsonify({'message': 'Username available'}), 200
+    else:
+        return jsonify({'message': 'Username do not exist'}), 409
 
-# Main windows of FlashCard section of app
+# Function for checing if user credentials are valid or not.
+@app.route('/check-credentials', methods=['POST'])
+def checkCredentials():
+    collection = db["users"]
+    username = request.form.get('username')
+    password = request.form.get('password')
+    print("User password : " + password, username)
+    user = collection.find_one({'username': username})
+    # print(user['password'])
+    if user:
+        if password == user['password']:
+            return jsonify({'message': 'Valid Creds'}), 200
+        else:
+            return jsonify({'message': 'Incorrect Password'}), 400
+  
+    else:
+        return jsonify({'message': 'Username not available'}), 400
 
-@app.route('/flash_card')
-def flashCardApp():
-    categories = ["Literature", "Mathematics", "Science", "History", "Geography", "Computer Science", "Art", "Music", "Health", "Business", "Test Preparation", "Miscellaneous"]
-
-    # questions_answers = [
-    # ("What is the capital of India?", "New Delhi"),
-    # ("What is the capital of Australia?", "Gundi Gathiya")
-    # ]
+def createFlashCard(name, category, flashcard_data):
     collection = db["flashcards"]
-    flashcards = collection.find({})
+    new_flashcard_pack = {
+        "name" : name,
+        "category" : category,
+        # "created_by": ObjectId("your_user_object_id"),  # Replace with actual ObjectId of the user
+        "created_at": datetime.utcnow().strftime('%Y-%m-%d'),
+        "times_opened": 0,
+        "flashcard_data": flashcard_data
+    }
+    insert_result = collection.insert_one(new_flashcard_pack)
 
-    print(flashcards)
-    # id = createFlashCard("A","B",questions_answers)
-    return render_template('flash_cards.html', id=id, flashcards=flashcards)
+    # Retrieve ObjectId of inserted document
+    id = insert_result.inserted_id
+
+    return id
 
 # Supporting function for fetching specific flashcard
-
 def fetch_flashcard_by_id(flashcard_id):
     collection = db["flashcards"]
     flashcard_object_id = ObjectId(flashcard_id)
@@ -216,19 +125,7 @@ def fetch_flashcard_by_id(flashcard_id):
     print(type(flashcard))
     return flashcard
 
-# Function for opening a particular flashcard
-
-@app.route('/flashcard/<flashcard_id>')
-def view_flashcard(flashcard_id):
-    flashcard = fetch_flashcard_by_id(flashcard_id)
-    return render_template('view_flashcard.html', flashcard=flashcard)
-
-#
-# Everything for Dashboard goes here
-# 
-
 # Function to fetch object of user from its object_id
-
 def fetch_user_by_id(user_id):
     collection = db["users"]
     user_object_id = ObjectId(user_id)
@@ -236,8 +133,133 @@ def fetch_user_by_id(user_id):
     print(type(user))
     return user
 
-# Function for add friend fuctionality
+def get_requests_for_receiver(receiver_id):
+    collection = db["requests"]
+    receiver_object_id = ObjectId(receiver_id)
+    receiver = fetch_user_by_id(receiver_object_id)
+    print(receiver, "It is receiver")
+    requests = list(collection.find({"receiver": receiver['username'], "status": "pending"}))
+    print(requests)
+    return requests
 
+
+def get_requests_sender_or_receiver(object_id):
+    collection = db["requests"]
+    object_id = ObjectId(object_id)
+    requests = list(collection.find({"$or": [{"sender": object_id}, {"receiver": object_id}]}))
+    return requests
+
+
+
+
+
+
+
+# 
+# Main Entry Point of Program 
+# 
+@app.route('/')
+def index():
+    return render_template('home_alt.html')
+
+
+
+
+
+#
+# Code for Login and Homepage
+#
+
+# Code to handle login request
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    collection = db["users"]
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print(username, password)
+        user = collection.find_one({'username': username})
+        requests = get_requests_for_receiver(str(user['_id']))
+        # print(requests)
+        print(user)
+        return render_template('dashboard.html', user=user, requests=requests)
+    else:
+        return render_template('home_alt.html')
+
+
+
+
+
+# 
+# Code For FlashCard Goes Here
+# 
+@app.route('/insert-flash-card', methods=['POST'])
+def createFlashCard():
+    data = []
+    name = request.form['name']
+    subject = request.form['subject']
+    questions = request.form.getlist('question[]')
+    answers = request.form.getlist('answer[]')
+    for i in range(len(min(questions,answers))):
+        data.append((questions[i], answers[i]))
+    print(data)
+    createFlashCard(name, subject, data)
+    return redirect(url_for('flashCardApp'))
+
+
+
+#
+# User Signup Stuff
+#
+
+# Function to Handle Signup
+@app.route('/sign-up', methods=['POST','GET'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = createUser(username, password)
+        user_id = str(user.inserted_id)
+        user = fetch_user_by_id(user_id)
+        print(user)
+        return render_template('home_alt.html')
+    else:
+        return render_template('signup.html')
+
+
+
+
+
+#
+# All code stuff for Flashcards goes here
+#
+
+# Main windows of FlashCard section of app
+@app.route('/flash_card')
+def flashCardApp():
+    categories = ["Literature", "Mathematics", "Science", "History", "Geography", "Computer Science", "Art", "Music", "Health", "Business", "Test Preparation", "Miscellaneous"]
+
+    collection = db["flashcards"]
+    flashcards = collection.find({})
+
+    print(flashcards)
+    return render_template('flash_cards.html', id=id, flashcards=flashcards)
+
+# Function for opening a particular flashcard
+@app.route('/flashcard/<flashcard_id>')
+def view_flashcard(flashcard_id):
+    flashcard = fetch_flashcard_by_id(flashcard_id)
+    return render_template('view_flashcard.html', flashcard=flashcard)
+
+
+
+
+
+#
+# Hey Friends Functionality Goes Here...
+# 
+
+# Function for add friend fuctionality
 @app.route("/add-friend/<user_id>", methods=['POST'])
 def addFriend(user_id):
     user = fetch_user_by_id(user_id)
@@ -256,23 +278,7 @@ def addFriend(user_id):
         else:
             return jsonify({'message': 'Failed to add friend'}), 500
     
-
-def get_requests_for_receiver(receiver_id):
-    collection = db["requests"]
-    receiver_object_id = ObjectId(receiver_id)
-    receiver = fetch_user_by_id(receiver_object_id)
-    print(receiver, "It is receiver")
-    requests = list(collection.find({"receiver": receiver['username'], "status": "pending"}))
-    print(requests)
-    return requests
-
-
-def get_requests_sender_or_receiver(object_id):
-    collection = db["requests"]
-    object_id = ObjectId(object_id)
-    requests = list(collection.find({"$or": [{"sender": object_id}, {"receiver": object_id}]}))
-    return requests
-
+# Function to accept friend request
 @app.route("/accept-friend-request/<request_id>", methods=['POST'])
 def accept_friend_request(request_id):
     # Assuming you have a requests collection in your database
@@ -298,6 +304,8 @@ def accept_friend_request(request_id):
         return jsonify({'message': 'Friend request accepted successfully'}), 200
     else:
         return jsonify({'message': 'Friend request not found'}), 404
+    
+# Function to reject friend request
 @app.route("/reject-friend-request/<request_id>", methods=['POST'])
 def reject_friend_request(request_id):
     collection = db["requests"]
@@ -306,33 +314,39 @@ def reject_friend_request(request_id):
     collection.update_one({"_id": request_object_id}, {"$set": {"status": "rejected"}})
     return jsonify({'message': 'Friend request rejected successfully'}), 200
 
-@app.route("/", methods=['POST'])
-def todo1():
-    if request.method == "POST":   # if the request method is post, then insert the todo document in todos collection
-        content = request.form['content']
-        degree = request.form['degree']
-        todos.insert_one({'content': content, 'degree': degree})
-        return redirect(url_for('todo1')) # redirect the user to home page
-    all_todos = todos.find()    # display all todo documents
-    return render_template('dashboard.html', todos = all_todos) # render home page template with all todos
-#Delete Route
-@app.post("/<id>/delete/")
-def delete(id): #delete function by targeting a todo document by its own id
-    todos.delete_one({"_id":ObjectId(id)}) #deleting the selected todo document by its converted id
-    return redirect(url_for('todo1')) # again, redirecting you to the home page 
-db = client.LearnSyncDatabase # creating your flask database using your mongo client 
-todos = db.todos # creating a collection called "todos"
-
 @app.route('/update-daily-task', methods=['POST'])
 def updateDailyTasks():
-    data = []
-    tasks = request.form.getlist('task[]')
-    for i in range(len(tasks)):
-        data.append(tasks[i])
-    print(data)
-    createFlashCard(name, subject, data)
-    return redirect(url_for('flashCardApp'))
+    pass
+
+# Function for sharing user stats with homepage
+def fetch_user_stats(user_id):
+    # Connect to MongoDB
+
+    # Query the users collection to get the user's stats
+    users_collection = db["users"]
+    user = users_collection.find_one({"_id": ObjectId(user_id)})
+
+    # Check if the user exists
+    if user:
+        # Extract and return the user stats
+        user_stats = user.get("stats", {})
+        return user_stats
+    else:
+        # Return None if the user is not found
+        return None
+
+@app.route('/getUserStats', method=['POST'])
+def getUserStats():
+    user_stats = fetch_user_stats()
+    return jsonify(user_stats)
 
 
+
+
+
+
+# 
+# Main() function of app
+# 
 if __name__ == '__main__':
     app.run(debug=True, port=8888)
